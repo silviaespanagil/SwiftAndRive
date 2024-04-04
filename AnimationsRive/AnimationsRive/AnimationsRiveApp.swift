77bdd7ff8c6123164c27ac7d32f7ab040a4cc240
Silvia España <silviaespanagil@gmail.com>
2024-04-04 14:48:48 +0200

    First commit

---
+++ AnimationsRive/AnimationsRive/AnimationsRiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsRiveApp.swift
+//  AnimationsRive
+//
+//  Created by Silvia España Gil on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsRiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
