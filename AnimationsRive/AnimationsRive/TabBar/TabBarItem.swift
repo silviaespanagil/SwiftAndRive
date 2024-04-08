b9cd91fd67e09daeaf1ece4adb3abe3fa8075627
Silvia España <silviaespanagil@gmail.com>
2024-04-08 18:25:41 +0200

    Create TabBarItem Entity

---
+++ AnimationsRive/AnimationsRive/TabBar/TabBarItem.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarItem.swift
+//  AnimationsRive
+//
+//  Created by Silvia España Gil on 8/4/24.
+//
+
+import Foundation
+import RiveRuntime
+
+struct TabBarItem: Identifiable {
+    
+    let id = UUID()
+    let icon: RiveViewModel
+}
