704e81181af82de9962db42b577107e8b6005971
Silvia España <silviaespanagil@gmail.com>
2024-04-09 09:30:34 +0200

    Create a Tab enum to manage all tabs cases

---
+++ AnimationsRive/AnimationsRive/TabBar/Enumerations/Tab.swift
@@ -0,0 +1,17 @@
+//
+//  Tab.swift
+//  AnimationsRive
+//
+//  Created by Silvia España Gil on 9/4/24.
+//
+
+import Foundation
+
+enum Tab: String {
+    
+    case chat
+    case search
+    case timer
+    case bell
+    case user
+}
