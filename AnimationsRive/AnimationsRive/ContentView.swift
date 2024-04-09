8106237ebe2543a39b38197ce3ef195d32784173
Silvia España <silviaespanagil@gmail.com>
2024-04-09 10:52:10 +0200

    Add viewBuilder for the menuButton

--- AnimationsRive/AnimationsRive/ContentView.swift
+++ AnimationsRive/AnimationsRive/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    
+    @State var isMenuOpen = false
+    
+    let menuButton = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     
     var body: some View {
         ZStack {
@@ -26,8 +31,26 @@ struct ContentView: View {
             case .user:
                 Text("user")
             }
+            
+            menuButtonView
+            
             TabBar()
         }
+    }
+    
+    @ViewBuilder
+    var menuButtonView: some View {
+        
+        menuButton.view()            .frame(width: 54, height: 54)
+            .mask(Circle())
+            .shadow(color: Color("Shadow").opacity(0.2),
+                    radius: 5, x: 0, y: 5)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding()
+            .onTapGesture {
+                isMenuOpen.toggle()
+                menuButton.setInput("isOpen", value: isMenuOpen)
+            }
     }
 }
 
