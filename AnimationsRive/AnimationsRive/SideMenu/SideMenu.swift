825b1b306d0ea02c80ee565199f9c8f4c5dc2fe1
Silvia España <silviaespanagil@gmail.com>
2024-04-10 13:34:09 +0200

    Create SideMenu view

---
+++ AnimationsRive/AnimationsRive/SideMenu/SideMenu.swift
@@ -0,0 +1,207 @@
+//
+//  SideMenu.swift
+//  AnimationsRive
+//
+//  Created by Silvia España Gil on 9/4/24.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct SideMenu: View {
+    
+    @AppStorage("selectedMenu") var selectedMenu: SelectedMenu = .home
+    
+    @State var isDarkMode = false
+    
+    var menuItems = [
+        MenuItem(text: "Home", icon: RiveViewModel(fileName: "icons", stateMachineName: "HOME_interactivity", artboardName: "HOME"), menu: .home),
+        MenuItem(text: "Search", icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH"), menu: .search),
+        MenuItem(text: "Favorites", icon: RiveViewModel(fileName: "icons", stateMachineName: "STAR_Interactivity", artboardName: "LIKE/STAR"), menu: .favorites),
+        MenuItem(text: "Help", icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT"), menu: .help)
+    ]
+
+    var menuItems2 = [
+        MenuItem(text: "History", icon: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER"), menu: .history),
+        MenuItem(text: "Notifications", icon: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL"), menu: .notifications)
+    ]
+
+    var menuItems3 = [
+        MenuItem(text: "Dark Mode", icon: RiveViewModel(fileName: "icons", stateMachineName: "SETTINGS_Interactivity", artboardName: "SETTINGS"), menu: .darkmode)
+    ]
+
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            
+            userArea
+                .padding()
+            
+            browseArea
+            
+            historyArea
+            
+            Spacer()
+            
+            toggleButton
+        }
+        .foregroundColor(.white)
+        .frame(maxWidth: 288, maxHeight: .infinity)
+        .background(Color(hex: "17203A"))
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color(hex: "17203A").opacity(0.3), radius: 40, x: 0, y: 20)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    var userArea: some View {
+        
+        HStack {
+            
+            Image(systemName: "person")
+                .padding(12)
+                .background(.white.opacity(0.2))
+                .mask(Circle())
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Meng To")
+                Text("UI Designer")
+                    .font(.subheadline)
+                    .opacity(0.7)
+            }
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    var browseArea: some View {
+        
+        Text("BROWSE")
+            .font(.subheadline).bold()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 24)
+            .padding(.top, 40)
+            .opacity(0.7)
+        
+        VStack(alignment: .leading, spacing: 0) {
+            
+            ForEach(menuItems) { item in
+                
+                Rectangle()
+                    .frame(height: 1)
+                    .opacity(0.1)
+                    .padding(.horizontal, 16)
+                
+                HStack(spacing: 14) {
+                    item.icon.view()
+                        .frame(width: 32, height: 32)
+                        .opacity(0.6)
+                    Text(item.text)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(12)
+                .background(
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.blue)
+                        .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                )
+                .background(Color("Background 2"))
+                .onTapGesture {
+                    withAnimation(.timingCurve(0.2, 0.8, 0.2, 1)) {
+                        selectedMenu = item.menu
+                    }
+                    item.icon.setInput("active", value: true)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        item.icon.setInput("active", value: false)
+                    }
+                }
+            }
+        }
+        .font(.headline)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(8)
+    }
+    
+    @ViewBuilder
+    var historyArea: some View {
+        
+        Text("HISTORY")
+            .font(.subheadline).bold()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 24)
+            .padding(.top, 40)
+            .opacity(0.7)
+        
+        VStack(alignment: .leading, spacing: 0) {
+            
+            ForEach(menuItems2) { item in
+                
+                Rectangle()
+                    .frame(height: 1)
+                    .opacity(0.1)
+                    .padding(.horizontal, 16)
+                
+                HStack(spacing: 14) {
+                    item.icon.view()
+                        .frame(width: 32, height: 32)
+                        .opacity(0.6)
+                    Text(item.text)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(12)
+                .background(
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.blue)
+                        .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                )
+                .background(Color("Background 2"))
+                .onTapGesture {
+                    withAnimation(.timingCurve(0.2, 0.8, 0.2, 1)) {
+                        selectedMenu = item.menu
+                    }
+                    item.icon.setInput("active", value: true)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        item.icon.setInput("active", value: false)
+                    }
+                }
+            }
+        }
+        .font(.headline)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(8)
+    }
+    
+    @ViewBuilder
+    var toggleButton: some View {
+        
+        HStack(spacing: 14) {
+            
+            menuItems3[0].icon.view()
+                .frame(width: 32, height: 32)
+                .opacity(0.6)
+                .onChange(of: isDarkMode) {
+                    if isDarkMode {
+                        menuItems3[0].icon.setInput("active", value: true)
+                    } else {
+                        menuItems3[0].icon.setInput("active", value: false)
+                    }
+                }
+            
+            Text(menuItems3[0].text)
+            
+            Toggle("", isOn: $isDarkMode)
+        }
+        .font(.headline)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(12)
+        .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding(8)
+    }
+}
+
+#Preview {
+    SideMenu()
+}
