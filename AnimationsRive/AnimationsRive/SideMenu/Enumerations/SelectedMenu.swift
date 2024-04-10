88cc7e360745086fc858a5d93b817f09f63572ed
Silvia España <silviaespanagil@gmail.com>
2024-04-10 13:33:57 +0200

    Create enum for the selectedMenu cases

---
+++ AnimationsRive/AnimationsRive/SideMenu/Enumerations/SelectedMenu.swift
@@ -0,0 +1,19 @@
+//
+//  SelectedMenu.swift
+//  AnimationsRive
+//
+//  Created by Silvia España Gil on 10/4/24.
+//
+
+import Foundation
+
+enum SelectedMenu: String {
+    
+    case home
+    case search
+    case favorites
+    case help
+    case history
+    case notifications
+    case darkmode
+}
